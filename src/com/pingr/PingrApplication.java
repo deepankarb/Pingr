5772b9307acda1a31119d20107da932815c72697
Deepankar Bhardwaj <deepankar.bhardwaj@gmail.com>
2013-06-10 11:41:02 +0530

    Syncing.

---
+++ src/com/pingr/PingrApplication.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.pingr;
+
+import android.app.Application;
+
+/**
+ * @author bharddee Singleton application class
+ * 
+ */
+public class PingrApplication extends Application {
+	private static PingrApplication instance;
+
+	public static PingrApplication getInstance() {
+		return instance;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		instance = this;
+	}
+}
---
+++ src/com/pingr/SettingsActivity.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.pingr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * @author iceman
+ *
+ */
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+    
+    
+}
